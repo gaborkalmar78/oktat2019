c001a7820df3b21c55ce07f353923b9c75cbf1d0
FataID <fata.imre@gmail.com>
2019-10-15 19:04:41 +0200

    temp01

---
+++ szilard/calculator/enumOperand.cs
@@ -0,0 +1,13 @@
+using System;
+namespace calculator
+{
+    public enum enumOperand
+    {
+
+        add = 1,
+        quit = 0,
+        Multiple = 3,
+        Substract = 2,
+        Division = 4
+    }
+}
