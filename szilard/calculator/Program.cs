cac13690e7ccff603e8306dcddcad36a1426bc9c
blade202 <56600379+blade202@users.noreply.github.com>
2019-10-15 18:39:47 +0200

    calculaor

---
+++ szilard/calculator/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetEnumOperand());
+
+
+
+
+        }
+
+        static enumOperand GetEnumOperand()
+        {
+            enumOperand resault = enumOperand.quit;
+            bool ok = false;
+            do
+            {
+                Console.Write("mit csináljakl?");
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "+":
+                        resault = enumOperand.add;
+                        ok = true;
+                        break;
+                    case "-":
+                        resault = enumOperand.Substract;
+                        ok = true;
+                        break;
+                    case "*":
+                        resault = enumOperand.Multiple;
+                        ok = true;
+                        break;
+                    case "/":
+                        resault = enumOperand.Division;
+                        ok = true;
+                        break;
+                    case "q":
+                        break;
+                    default:
+                        break;
+                }
+                return resault;
+            } while (ok);
+
+
+        }
+    }
+}
+
+
+
