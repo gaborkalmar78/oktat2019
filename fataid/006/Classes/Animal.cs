5d037b3f13e84f6d33b7fc49de9ac5e19237fe9d
FataID <fata.imre@gmail.com>
2019-10-22 19:31:04 +0200

    step 01

---
+++ fataid/006/Classes/Animal.cs
@@ -0,0 +1,16 @@
+namespace _006.Classes
+{
+    public class Animal
+    {
+        private int age = 0;
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public void Live()
+        {
+            age += 1;
+        }
+    }
+}
---
+++ fataid/006/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _006
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Classes.Animal animal = new Classes.Animal();
+            Console.WriteLine((animal.Age).ToString());
+        }
+    }
+}
