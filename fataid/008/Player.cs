aae125e1428b04064d957665ab3b4c9b7d4a0c55
FataID <fata.imre@gmail.com>
2019-11-26 17:09:52 +0100

    leftover from last course

--- fataid/008/Player.cs
+++ fataid/008/Player.cs
@@ -1,10 +1,28 @@
 using System.Collections.Generic;
-public class Player
+
+namespace _008
 {
-    private List<Card> cards = new List<Card>();
-    public List<Card> Cards
+    public class Player
     {
-        get { return cards; }
-        //set { cards = value; }
+        public string Name { get; set; }
+        private List<Card> cards = new List<Card>();
+        public List<Card> Cards
+        {
+            get { return cards; }
+            set { cards = value; }
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+        public string ToHtml()
+        {
+            string CardCells = "";
+            for (int i = 0; i < Cards.Count; i++)
+            {
+                CardCells += $"<td>{Cards[i].ToHtml()}</td>";
+            }
+            return CardCells;
+        }
     }
 }
--- fataid/008/Program.cs
+++ fataid/008/Program.cs
@@ -1,36 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 
-public class Program
+namespace _008
 {
-    public static void Main()
+    public class Program
     {
-        Card[] cards = new Card[] {
-            new Card() {Name = "car1", MaxSpeed = 0, Weight = 0, Price = 0, Brand = "", Engine = 0},
-            new Card() {Name = "car2", MaxSpeed = 0, Weight = 0, Price = 0, Brand = "", Engine = 0},
-            new Card() {Name = "car3", MaxSpeed = 0, Weight = 0, Price = 0, Brand = "", Engine = 0},
-            new Card() {Name = "car4", MaxSpeed = 0, Weight = 0, Price = 0, Brand = "", Engine = 0},
-            new Card() {Name = "car5", MaxSpeed = 0, Weight = 0, Price = 0, Brand = "", Engine = 0},
-            new Card() {Name = "car6", MaxSpeed = 0, Weight = 0, Price = 0, Brand = "", Engine = 0},
-            new Card() {Name = "car7", MaxSpeed = 0, Weight = 0, Price = 0, Brand = "", Engine = 0},
-            new Card() {Name = "car8", MaxSpeed = 0, Weight = 0, Price = 0, Brand = "", Engine = 0},
-        };
-        Game game = new Game(null, null);
-        //Console.WriteLine("Hello World");
+        public static void Main()
+        {
+            // Game game = new Game(null, null);
+            Game game = new Game();
+            game.Play();
+            //Console.WriteLine("Hello World");
+        }
     }
-}
 
 
-public class genericType<T>
-{
-    public int Count(T[] items)
-    {
-        return items.Length;
+    // public class genericType<T>
+    // {
+    //     public int Count(T[] items)
+    //     {
+    //         return items.Length;
 
-    }
-    public T First(T[] items)
-    {
-        return items[0];
+    //     }
+    //     public T First(T[] items)
+    //     {
+    //         return items[0];
 
-    }
+    //     }
+    // }
 }
