1d44e30b70199f5564319cef98371117f06745e9
blade202 <56600379+blade202@users.noreply.github.com>
2019-12-10 20:01:43 +0100

    komit

---
+++ szilard/card game/Player.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace card_game
+{
+    public class Player
+    {
+        public string PlayerName { get; set; }
+        public List<Card> Hand { get; set; }
+
+    }
+}
